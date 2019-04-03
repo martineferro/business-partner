3f15d3fec1abd00a582c23a18e8257e0f61e1028
Lekealem Asong <lekealemasong@yahoo.de>
2019-04-03 13:26:46 +0200

    Add production webpack

---
+++ webpack.production.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+  },
+  output: {
+    path: path.resolve(__dirname, './src/server/static'),
+    publicPath: '/static',
+    filename: 'components/[name]-bundle.js',
+    library: 'business-partner-[name]',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+
+  //exclude empty dependencies, require for Joi
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+
+  bail: true,
+  // devtool: 'source-map',
+
+  plugins: [
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|de/)
+  ],
+
+  resolve: {
+    extensions: ['.json', '.jsx', '.js']
+  },
+
+  resolveLoader: {
+    extensions: ['.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: [
+          path.join(__dirname, 'src')
+        ],
+        options: {
+          presets: [
+            ['es2015', {modules: false}],
+            'react',
+            'stage-0'
+          ],
+          plugins: ['transform-decorators-legacy']
+        }
+      }
+    ]
+  }
+};
