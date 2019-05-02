fb79e5a20bf23f3fccb11320bc14e06e212d4bd4
Lekealem Asong <lekealemasong@yahoo.de>
2019-05-02 14:18:28 +0200

    Add customer-admin role handling to user abilitie

--- src/client/models/UserAbilities.js
+++ src/client/models/UserAbilities.js
@@ -4,7 +4,7 @@ class UserAbilities {
   constructor(roles) {
     if (roles.includes('admin')) {
       this.abilitiesForRole = abilities['admin'];
-    } else if (roles.includes('supplier-admin')) {
+    } else if (roles.includes('supplier-admin') || roles.includes('customer-admin')) {
       this.abilitiesForRole = abilities['business-partner-admin'];
     } else {
       this.abilitiesForRole = abilities['business-partner'];
